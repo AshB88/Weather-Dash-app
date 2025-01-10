349231c01e068a6aa3caf14ccae44c82f0cf83fb
Ash <ashleighb.jjd@gmail.com>
2025-01-09 23:59:02 -0500

    Added to weatherService.ts

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,14 +2,70 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state: string;
+}
 
 // TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  date: string;
+  icon: string;
+  iconDescription: string;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+
+  constructor(
+    city: string,
+    date: string,
+    icon: string,
+    iconDescription: string,
+    tempF: number,
+    windSpeed: number,
+    humidity: number,
+
+  ) {
+    this.city = city;
+    this.date = date;
+    this.icon = icon;
+    this.iconDescription = iconDescription;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity;
+
+  }
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL?: string;
+  private apiKey?: string;
+  private city = '';
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL;
+    this.apiKey = process.env.API_KEY;
+  }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+    try {
+      if (!this.baseURL || !this.apiKey) {
+        throw new Error('API base URL or API key not found');
+      }
+      const response: Coordinates[]
+        = await fetch(query).then((res) => res.json());
+      return response[0];
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method
