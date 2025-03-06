555f370847f7bdb3fa14bfac6fef3c3b19704935
Ash <ashleighb.jjd@gmail.com>
2025-03-06 15:21:37 -0500

    Updated weatherService.ts

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,18 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
- import HistoryService from '../../service/historyService.js';
- import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', async (req: Request, res: Response) => {
+router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
   try {
     const cityName = req.body.cityName;
 
+    weatherService.getWeatherForCity(cityName).then((data) => {
   // TODO: save city to search history
-    WeatherService.getWeatherForCity(cityName).then((data) => {
       HistoryService.addCity(cityName);
-
       res.json(data);
     });
   } catch (error) {
@@ -22,26 +21,26 @@ router.post('/', async (req: Request, res: Response) => {
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {
-  HistoryService.getCities()
-    .then((data) => {
+router.get('/history', async (_req: Request, res: Response) => {
+  try {
+    HistoryService.getCities().then((data) => {
       return res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
     });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req: Request, res: Response) => {
   try {
     if (!req.params.id) {
-      res.status(400).json({ error: 'City ID is required' });
+      res.status(400).json({ error: 'City ID required' });
       return;
     }
 
     await HistoryService.removeCity(req.params.id);
-    res.json({ success: 'The city has been removed from search history' });
+    res.json({ success: 'City removed from search history' });
   } catch (error) {
     res.status(500).json(error);
   }
